a96b3bfec47978e41f307b596bab254df8336f05
nicephil <nicephil@gmail.com>
2020-03-05 22:25:20 +0800

    restart always

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -tid --name firefox -p 8087:8083 -p 5900:5900 oldiy/chrome-novnc:latest
+docker run -tid --restart=always --name firefox -p 8087:8083 -p 5900:5900 oldiy/chrome-novnc:latest
